9f81af7b972396b1cf0a32b359751df0d5d80a61
Maychell <maychellfernandes@hotmail.com>
2017-02-05 17:22:28 -0300

    finish the robot program

--- RandomWalk.java
+++ RandomWalk.java
@@ -1,11 +1,21 @@
 import java.util.Random;
 
 class RandomWalk extends Triangule {
-  private final int MIN_RANDOM = 1;
-  private final int MAX_RANDOM = 2;
+  private final int MIN_RANDOM              = 1;
+  private final int MAX_RANDOM              = 7;
+  private final int CHANGE_NOTHING          = 1;
+  private final int TURN_RIGHT 	            = 2;
+  private final int TURN_LEFT               = 3;
+  private final int VERTICAL_HORIENTATION   = 4;
+  private final int HORIZONTAL_HORIENTATION = 5;
+  private final int FORWARDS_DIRECTION      = 6;
+  private final int BACKWARDS_DIRECTION     = 7;
 
   public RandomWalk(int width, int height, int colour, int speed) {
     super(width, height, colour, speed);
+
+    setHorientation(VERTICAL);
+    setDirection(FORWARDS);
   }
 
   public void move() {
@@ -18,25 +28,43 @@ class RandomWalk extends Triangule {
   }
 
   private void moveRamdomly() {
-    int newHorientation = generateRandomWalk();
-    int newDirection    = generateRandomWalk();
+  	int movement_id = generateRandom();
 
-    setHorientation(newHorientation);
-    setDirection(newDirection);
+  	switch(movement_id) {
+  		case TURN_RIGHT:
+  			turnRight();
+  			break;
+  		case TURN_LEFT:
+  			turnLeft();
+  			break;
+  		case VERTICAL_HORIENTATION:
+  			setHorientation(VERTICAL);
+  			break;
+  		case HORIZONTAL_HORIENTATION:
+  			setHorientation(HORIZONTAL);
+  			break;
+  		case FORWARDS_DIRECTION:
+  			setDirection(FORWARDS);
+  			break;
+  		case BACKWARDS_DIRECTION:
+  			setDirection(BACKWARDS);
+  			break;
+  	}
 
     checkIfHasReached();
   }
 
-  private int generateRandomWalk() {
+  private int generateRandom() {
     Random rand = new Random();
     return rand.nextInt(MAX_RANDOM) + MIN_RANDOM;
   }
 
   public void checkIfHasReached() {
-    if(isForwards() && (hasReachedMaxHeight() || hasReachedMaxWidth()))
-      setDirection(BACKWARDS);
-
-    if(!isForwards() && (hasReachedMinHeight() || hasReachedMinWidth()))
-      setDirection(FORWARDS);
+    if(hasReachedMaxHeight() || hasReachedMinHeight() || hasReachedMinWidth() || hasReachedMaxWidth()) {
+    	if(isForwards())
+      	setDirection(BACKWARDS);
+      else
+      	setDirection(FORWARDS);
+    }
   }
 }
--- tester.java
+++ tester.java
@@ -1,7 +1,7 @@
 private final int ALICE_COLOUR = 200;
 private final int ALICE_SPEED  = 10;
 private final int BOB_COLOUR   = 10;
-private final int BOB_SPEED    = 5;
+private final int BOB_SPEED    = 20;
 
 private Triangule alice, bob;
 
@@ -9,7 +9,7 @@ void setup() {
   size(1000, 500);
   background(255);
   alice = new Patroller(30, 20, ALICE_COLOUR, ALICE_SPEED);
-  bob   = new RandomWalk(20, 30, BOB_COLOUR, BOB_SPEED);
+  bob   = new RandomWalk(30, 20, BOB_COLOUR, BOB_SPEED);
 }
 
 void draw() {
